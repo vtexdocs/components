7b01df57a254c736f647933162da1ee3d41624b7
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-10-23 11:30:00 -0300

    Update index.mjs

--- dist/index.mjs
+++ dist/index.mjs
@@ -6497,25 +6497,7 @@ var es_default = {
 };
 
 // src/messages/pt.json
-var pt_default = {
-  "feedback_section.question": "Was this helpful?",
-  "feedback_section.response": "Thanks for your feedback.",
-  "feedback_section.positive": "Yes",
-  "feedback_section.negative": "No",
-  "feedback_section.edit": "Suggest edits (Github)",
-  "feedback_modal.title": "Leave a comment (optional)",
-  "feedback_modal.button": "Send Feedback",
-  "search_input.empty": "No results found. Try different search terms.",
-  "search_input.placeholder": "Search",
-  "sidebar_search.placeholder": "Search in",
-  "image.error_loading": "An error occurred while trying to load the image.",
-  api_reference_sidebar_filter_clear: "Clear all",
-  api_reference_sidebar_filter: "Filter by",
-  "cookie_bar.title": "We use cookies",
-  "cookie_bar.description": "This website uses cookies to improve site navigation, analyze website usage, and assist in our marketing efforts.",
-  "cookie_bar.accept": "I understand",
-  "cookie_bar.decline": "I decline"
-};
+var pt_default = src/messages/pt.json;
 
 // src/utils/get-message.ts
 var getMessages = () => {
