9ca19ac4058b52236266221d47bb1fcd42e45f68
Carolina Menezes <carolina.menezes@vtex.com.br>
2025-11-27 19:42:55 -0300

    feat: export icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,43 @@
+// Documentation Category Icons
+export { default as APIGuidesIcon } from './api-guides-icon'
+export { default as APIReferenceIcon } from './api-reference-icon'
+export { default as AppDevelopmentIcon } from './app-development-icon'
+export { default as ReleaseNotesIcon } from './release-notes-icon'
+export { default as StorefrontDevelopmentIcon } from './storefront-development-icon'
+export { default as TroubleshootingIcon } from './troubleshooting-icon'
+export { default as VTEXIOAppsIcon } from './vtex-io-apps-icon'
+
+// Status/Change Icons
+export { default as AddedIcon } from './added-icon'
+export { default as DeprecatedIcon } from './deprecated-icon'
+export { default as FixedIcon } from './fixed-icon'
+export { default as ImprovedIcon } from './improved-icon'
+export { default as RemovedIcon } from './removed-icon'
+
+// Social Media & Sharing Icons
+export { default as EmailIcon } from './email-icon'
+export { default as FacebookIcon } from './facebook-icon'
+export { default as LinkedinIcon } from './linkedin-icon'
+export { default as LinkIcon } from './link-icon'
+export { default as ShareIcon } from './share-icon'
+export { default as TwitterIcon } from './twitter-icon'
+
+// UI Action Icons
+export { default as CloseFilterIcon } from './close-filter-icon'
+export { default as CloseIcon } from './close-icon'
+export { default as CopyIcon } from './copy-icon'
+export { default as EditIcon } from './edit-icon'
+export { default as ExpandedResultsIcon } from './expanded-results-icon'
+export { default as SearchIcon } from './search-icon'
+export { default as SideBarToggleIcon } from './sidebar-toggle-icon'
+export { default as GridIcon } from './grid-icon'
+
+// Navigation Icons
+export { default as ArrowLeftIcon } from './arrow-left-icon'
+export { default as CaretIcon } from './caret'
+export { default as LongArrowIcon } from './long-arrow-icon'
+
+// Feedback Icons
+export { default as LikeIcon } from './like-icon'
+export { default as LikeSelectedIcon } from './like-selected-icon'
+
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,63 @@ export {
   Input,
   type Item,
 } from './lib'
+export { getDaysElapsed } from 'utils/get-days-elapsed'
 export { default as LibraryContextProvider } from 'utils/context/libraryContext'
 export { LibraryContext } from 'utils/context/libraryContext'
 export { default as SearchConfig } from 'utils/config/search-config'
+
+// Icons
+// Documentation Category Icons
+export {
+  APIGuidesIcon,
+  APIReferenceIcon,
+  AppDevelopmentIcon,
+  ReleaseNotesIcon,
+  StorefrontDevelopmentIcon,
+  TroubleshootingIcon,
+  VTEXIOAppsIcon,
+} from './components/icons'
+
+// Status/Change Icons
+export {
+  AddedIcon,
+  DeprecatedIcon,
+  FixedIcon,
+  ImprovedIcon,
+  RemovedIcon,
+} from './components/icons'
+
+// Social Media & Sharing Icons
+export {
+  EmailIcon,
+  FacebookIcon,
+  LinkedinIcon,
+  LinkIcon,
+  ShareIcon,
+  TwitterIcon,
+} from './components/icons'
+
+// UI Action Icons
+export {
+  CloseFilterIcon,
+  CloseIcon,
+  CopyIcon,
+  EditIcon,
+  ExpandedResultsIcon,
+  SearchIcon,
+  SideBarToggleIcon,
+} from './components/icons'
+
+// Navigation Icons
+export {
+  ArrowLeftIcon,
+  CaretIcon,
+  LongArrowIcon,
+  GridIcon,
+} from './components/icons'
+
+// Feedback Icons
+export {
+  LikeIcon,
+  LikeSelectedIcon,
+} from './components/icons'
